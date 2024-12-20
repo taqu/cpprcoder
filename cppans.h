b26587e6969921d34049096f12a27e76b53ad740
taqu <taqu2920.dev@gmail.com>
2024-12-20 18:39:52 +0900

    Fix encode/decode
    Add simd encode/decode

--- cppans.h
+++ cppans.h
@@ -6,6 +6,7 @@ Put '#define CPPANS_IMPLEMENTATION' before including this file to create the imp
 */
 #define CPPANS_IMPLEMENTATION (1)
 #include <cstdint>
+#include <immintrin.h>
 
 namespace cppans
 {
@@ -26,6 +27,10 @@ public:
     inline static constexpr u32 ProbBits = 14;
     inline static constexpr u32 ProbScale = 1 << ProbBits;
     inline static constexpr u32 rANSByteLowBounds = 1UL << 23;
+    inline static constexpr u32 WordLowBounds = 1UL<<16;
+    inline static constexpr u32 WordScaleBits = 12;
+    inline static constexpr u32 WordM = 1 << WordScaleBits;
+
     using State = u32;
 
     struct EncSymbol
@@ -37,10 +42,38 @@ public:
         u16 rcp_shift_;
     };
 
+    struct DecSymbol
+    {
+        u16 start_;
+        u16 freq_;
+    };
+
+    union WordSlot
+    {
+        u32 x_;
+        struct{
+            u16 freq_;
+            u16 bias_;
+        };
+    };
+    struct WordTables
+    {
+        WordSlot slots_[WordM];
+        u8 slot2symbol_[WordM];
+    };
+
+    union SimdState
+    {
+        __m128i simd_;
+        u32 lane_[4];
+    };
+
     static u64 calc_encoded_size(u32 size);
     static u32 encode(u32 dst_size, u8* dst, u32 src_size, const u8* src);
     static u32 decode(u32 dst_size, u8* dst, u32 src_size, const u8* src);
 
+    static u32 encode_simd(u32 dst_size, u8* dst, u32 src_size, const u8* src);
+    static u32 decode_simd(u32 dst_size, u8* dst, u32 src_size, const u8* src);
 private:
     rANS(const rANS&) = delete;
     rANS& operator=(const rANS&) = delete;
@@ -50,8 +83,9 @@ private:
 #ifdef CPPANS_IMPLEMENTATION
 #    include <cassert>
 #    include <cstring>
-#    include <immintrin.h>
-
+#ifdef _DEBUG
+#include <stdio.h>
+#endif
 #    if defined(_MSC_VER)
 #        define CPPANS_RESTRICT __restrict
 #    elif defined(__gnuc__)
@@ -215,6 +249,14 @@ namespace
         }
     }
 
+    void init(rANS::DecSymbol& symbol, u32 start, u32 freq)
+    {
+        assert(start <= (1 << 16));
+        assert(freq <= (1 << 16) - start);
+        symbol.start_ = static_cast<u16>(start);
+        symbol.freq_ = static_cast<u16>(freq);
+    }
+
     inline void init(rANS::State& state)
     {
         state = rANS::rANSByteLowBounds;
@@ -291,6 +333,160 @@ namespace
         r = x;
     }
 
+    rANS::State wordEncInit()
+    {
+        return rANS::WordLowBounds;
+    }
+
+    // Initialize slots for a symbol in the table
+void initSymbols(rANS::WordTables& tables, u8 sym, u32 start, u32 freq)
+{
+    for (u32 i=0; i < freq; ++i) {
+        u32 slot = start + i;
+        assert(slot<rANS::WordM);
+        tables.slot2symbol_[slot] = sym;
+        tables.slots_[slot].freq_ = static_cast<u16>(freq);
+        tables.slots_[slot].bias_ = static_cast<u16>(i);
+    }
+}
+
+void wordEncPut(rANS::State& r, u16*& ptr, u32 start, u32 freq)
+{
+    // renormalize
+    u32 x = r;
+    if (((rANS::WordLowBounds >> rANS::WordScaleBits) << 16) * freq <= x) {
+        ptr -= 1;
+        *ptr = static_cast<u16>(x & 0xFFFFUL);
+        x >>= 16;
+    }
+    // x = C(s,x)
+    r = ((x / freq) << rANS::WordScaleBits) + (x % freq) + start;
+}
+
+// Flushes the rANS encoder
+static inline void wordEncFlush(rANS::State& r, u16*& ptr)
+{
+    u32 x = r;
+    ptr -= 2;
+    ptr[0] = static_cast<u16>(x >> 0);
+    ptr[1] = static_cast<u16>(x >> 16);
+}
+
+// Initializes a rANS decoder.
+void wordDecInit(rANS::State& r, const u16*& ptr)
+{
+    r  = ptr[0] << 0;
+    r |= ptr[1] << 16;
+    ptr += 2;
+}
+
+// Decodes a symbol using the given tables.
+u8 wordDecSym(rANS::State& r, const rANS::WordTables& table)
+{
+    u32 x = r;
+    u32 slot = x & (rANS::WordM - 1);
+
+    // s, x = D(x)
+    r = table.slots_[slot].freq_ * (x >> rANS::WordScaleBits) + table.slots_[slot].bias_;
+    return table.slot2symbol_[slot];
+}
+
+// Renormalize after decoding a symbol.
+void wordDecRenorm(rANS::State& r, const u16*& ptr)
+{
+    u32 x = r;
+    if (x < rANS::WordLowBounds) {
+        r = (x << 16) | *ptr;
+        ptr += 1;
+    }
+}
+
+// Initializes a SIMD rANS decoder.
+void simdDecInit(rANS::SimdState& r, const u16*& ptr)
+{
+    r.simd_ = _mm_loadu_si128((const __m128i*)ptr);
+    ptr += 2*4;
+}
+
+// Decodes a four symbols in parallel using the given tables.
+u32 simdDecSym(rANS::SimdState& r, const rANS::WordTables& tables)
+{
+    __m128i freq_bias_lo, freq_bias_hi, freq_bias;
+    __m128i freq, bias;
+    __m128i xscaled;
+    __m128i x = r.simd_;
+    __m128i slots = _mm_and_si128(x, _mm_set1_epi32(rANS::WordM - 1));
+    u32 i0 = (u32) _mm_cvtsi128_si32(slots);
+    u32 i1 = (u32) _mm_extract_epi32(slots, 1);
+    u32 i2 = (u32) _mm_extract_epi32(slots, 2);
+    u32 i3 = (u32) _mm_extract_epi32(slots, 3);
+
+    // symbol
+    u32 s = tables.slot2symbol_[i0] | (tables.slot2symbol_[i1] << 8) | (tables.slot2symbol_[i2] << 16) | (tables.slot2symbol_[i3] << 24);
+
+    // gather freq_bias
+    freq_bias_lo = _mm_cvtsi32_si128(tables.slots_[i0].x_);
+    freq_bias_lo = _mm_insert_epi32(freq_bias_lo, tables.slots_[i1].x_, 1);
+    freq_bias_hi = _mm_cvtsi32_si128(tables.slots_[i2].x_);
+    freq_bias_hi = _mm_insert_epi32(freq_bias_hi, tables.slots_[i3].x_, 1);
+    freq_bias = _mm_unpacklo_epi64(freq_bias_lo, freq_bias_hi);
+
+    // s, x = D(x)
+    xscaled = _mm_srli_epi32(x, rANS::WordScaleBits);
+    freq = _mm_and_si128(freq_bias, _mm_set1_epi32(0xffff));
+    bias = _mm_srli_epi32(freq_bias, 16);
+    r.simd_ = _mm_add_epi32(_mm_mullo_epi32(xscaled, freq), bias);
+    return s;
+}
+
+// Renormalize after decoding a symbol.
+static inline void simdDecRenorm(rANS::SimdState& r, const u16*& ptr)
+{
+    static alignas(16) const int8_t shuffles[16][16] = {
+#define _ -1 // for readability
+        { _,_,_,_, _,_,_,_, _,_,_,_, _,_,_,_ }, // 0000
+        { 0,1,_,_, _,_,_,_, _,_,_,_, _,_,_,_ }, // 0001
+        { _,_,_,_, 0,1,_,_, _,_,_,_, _,_,_,_ }, // 0010
+        { 0,1,_,_, 2,3,_,_, _,_,_,_, _,_,_,_ }, // 0011
+        { _,_,_,_, _,_,_,_, 0,1,_,_, _,_,_,_ }, // 0100
+        { 0,1,_,_, _,_,_,_, 2,3,_,_, _,_,_,_ }, // 0101
+        { _,_,_,_, 0,1,_,_, 2,3,_,_, _,_,_,_ }, // 0110
+        { 0,1,_,_, 2,3,_,_, 4,5,_,_, _,_,_,_ }, // 0111
+        { _,_,_,_, _,_,_,_, _,_,_,_, 0,1,_,_ }, // 1000
+        { 0,1,_,_, _,_,_,_, _,_,_,_, 2,3,_,_ }, // 1001
+        { _,_,_,_, 0,1,_,_, _,_,_,_, 2,3,_,_ }, // 1010
+        { 0,1,_,_, 2,3,_,_, _,_,_,_, 4,5,_,_ }, // 1011
+        { _,_,_,_, _,_,_,_, 0,1,_,_, 2,3,_,_ }, // 1100
+        { 0,1,_,_, _,_,_,_, 2,3,_,_, 4,5,_,_ }, // 1101
+        { _,_,_,_, 0,1,_,_, 2,3,_,_, 4,5,_,_ }, // 1110
+        { 0,1,_,_, 2,3,_,_, 4,5,_,_, 6,7,_,_ }, // 1111
+#undef _
+    };
+    static uint8_t const numbits[16] = {
+        0,1,1,2, 1,2,2,3, 1,2,2,3, 2,3,3,4
+    };
+
+    __m128i x = r.simd_;
+
+    // NOTE: SSE2+ only offer a signed 32-bit integer compare, while we
+    // need unsigned. So we subtract 0x80000000 before the compare,
+    // which converts unsigned integers to signed integers in an
+    // order-preserving manner.
+    __m128i x_biased = _mm_xor_si128(x, _mm_set1_epi32((s32) 0x80000000));
+    __m128i greater = _mm_cmpgt_epi32(_mm_set1_epi32(rANS::WordLowBounds - 0x80000000), x_biased);
+    unsigned int mask = _mm_movemask_ps(_mm_castsi128_ps(greater));
+
+    // NOTE: this will read slightly past the end of the input buffer.
+    // In practice, either pad the input buffer by 8 bytes at the end,
+    // or switch to the non-SIMD version once you get close to the end.
+    __m128i memvals = _mm_loadl_epi64((const __m128i*)ptr);
+    __m128i xshifted = _mm_slli_epi32(x, 16);
+    __m128i shufmask = _mm_load_si128((const __m128i*)shuffles[mask]);
+    __m128i newx = _mm_or_si128(xshifted, _mm_shuffle_epi8(memvals, shufmask));
+    r.simd_ = _mm_blendv_epi8(x, newx, greater);
+    ptr += numbits[mask];
+}
+
 } // namespace
 
 u64 rANS::calc_encoded_size(u32 size)
@@ -326,11 +522,9 @@ u32 rANS::encode(u32 dst_size, u8* dst, u32 src_size, const u8* src)
     if(ptr < dst) {
         return 0;
     }
-    u32* u32ptr = reinterpret_cast<u32*>(ptr) + 1;
-    for(u32 i = 0; i < 257; ++i) {
-        u32ptr[i] = cum_freqs[i];
-    }
-    u32ptr[-1] = src_size;
+    u32* u32ptr = reinterpret_cast<u32*>(ptr);
+    ::memcpy(u32ptr+1, cum_freqs, sizeof(u32)*257);
+    ::memcpy(u32ptr, &src_size, sizeof(u32));
     u32 encoded_size = static_cast<u32>(dst + dst_size - ptr);
     return encoded_size;
 }
@@ -341,9 +535,12 @@ u32 rANS::decode(u32 dst_size, u8* dst, u32 src_size, const u8* src)
     assert(nullptr != dst);
     assert(0 < src_size);
     assert(nullptr != src);
-    assert(257 * sizeof(u32) <= src_size);
+    assert(258 * sizeof(u32) <= src_size);
     u32 original_size;
     ::memcpy(&original_size, src, sizeof(u32));
+    if(dst_size<original_size){
+        return 0;
+    }
     const u32* cum_freqs = reinterpret_cast<const u32*>(src) + 1;
     u8 cum2sym[ProbScale] = {};
     for(u32 s = 0; s < 256; ++s) {
@@ -352,7 +549,7 @@ u32 rANS::decode(u32 dst_size, u8* dst, u32 src_size, const u8* src)
         }
     }
 
-    const u8* ptr = src + sizeof(u32) * 257;
+    const u8* ptr = src + sizeof(u32) * 258;
     State rans;
     init_decode(rans, ptr);
 
@@ -362,9 +559,95 @@ u32 rANS::decode(u32 dst_size, u8* dst, u32 src_size, const u8* src)
         u32 freq = cum_freqs[s+1] - cum_freqs[s];
         advance(rans, ptr, cum_freqs[s], freq, ProbBits);
     }
-    u32 decoded_size = static_cast<u32>(ptr - (src+sizeof(u32) * 257));
+    u32 decoded_size = static_cast<u32>(ptr - (src+sizeof(u32) * 258));
+    return decoded_size;
+}
+
+
+u32 rANS::encode_simd(u32 dst_size, u8* dst, u32 src_size, const u8* src)
+{
+    assert(0 < dst_size);
+    assert(nullptr != dst);
+    assert(0 < src_size);
+    assert(nullptr != src);
+
+    u32 freqs[256];
+    count(freqs, src_size, src);
+    u32 cum_freqs[257];
+    cumulative(cum_freqs, freqs);
+    normalize(freqs, cum_freqs, rANS::WordM);
+
+    WordTables tables;
+    for(u32 s=0; s<256; ++s){
+        initSymbols(tables, static_cast<u8>(s), cum_freqs[s], freqs[s]);
+    }
+
+    State rans[8];
+    for(u32 i=0; i<8; ++i){
+        rans[i] = wordEncInit();
+    }
+
+    u16* ptr = reinterpret_cast<u16*>(dst + dst_size);
+    for(u32 i = src_size; 0 < i; --i) {
+        u8 s = src[i - 1];
+        wordEncPut(rans[(i - 1) & 7], ptr, cum_freqs[s], freqs[s]);
+    }
+    for(u32 i=8; 0<i; --i){
+        wordEncFlush(rans[i-1], ptr);
+    }
+    u8* header = reinterpret_cast<u8*>(ptr) - sizeof(u32) * 258;
+    if(header < dst) {
+        return 0;
+    }
+    u32* u32ptr = reinterpret_cast<u32*>(header);
+    ::memcpy(u32ptr+1, cum_freqs, sizeof(u32)*257);
+    ::memcpy(u32ptr, &src_size, sizeof(u32));
+    u32 encoded_size = static_cast<u32>(dst + dst_size - header);
+    return encoded_size;
+}
+
+u32 rANS::decode_simd(u32 dst_size, u8* dst, u32 src_size, const u8* src)
+{
+    assert(0 < dst_size);
+    assert(nullptr != dst);
+    assert(0 < src_size);
+    assert(nullptr != src);
+    assert(258 * sizeof(u32) <= src_size);
+    u32 original_size;
+    ::memcpy(&original_size, src, sizeof(u32));
+    if(dst_size<original_size){
+        return 0;
+    }
+    const u32* cum_freqs = reinterpret_cast<const u32*>(src) + 1;
+
+    WordTables tables;
+    for(u32 s=0; s<256; ++s){
+        initSymbols(tables, static_cast<u8>(s), cum_freqs[s], cum_freqs[s+1]-cum_freqs[s]);
+    }
+
+    const u16* ptr = reinterpret_cast<const u16*>(src + sizeof(u32) * 258);
+    SimdState rans0,rans1;
+    simdDecInit(rans0, ptr);
+    simdDecInit(rans1, ptr);
+
+    u32 simdSize = original_size & ~0x07UL;
+
+    for(u32 i = 0; i < simdSize; i += 8) {
+        u32 s03 = simdDecSym(rans0, tables);
+        u32 s47 = simdDecSym(rans1, tables);
+        *(u32*)(dst + i) = s03;
+        *(u32*)(dst + i + 4) = s47;
+        simdDecRenorm(rans0, ptr);
+        simdDecRenorm(rans1, ptr);
+    }
+    for(u32 i = simdSize; i < original_size; ++i) {
+        SimdState* which = (i & 4) != 0 ? &rans1 : &rans0;
+        uint8_t s = wordDecSym(which->lane_[i & 3], tables);
+        dst[i] = s;
+    }
     return original_size;
 }
+
 } // namespace cppans
 #endif
 #endif INC_CPPANS_H_
